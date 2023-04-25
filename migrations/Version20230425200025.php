6235ac99af2e389f24baef12878a838c6309ce04
AndrewRafel <40300451+AndrewRafel@users.noreply.github.com>
2023-04-25 22:13:18 +0200

    Show objects

--- migrations/Version20230425200025.php
+++ migrations/Version20230425200025.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230421213008 extends AbstractMigration
+final class Version20230425200025 extends AbstractMigration
 {
     public function getDescription(): string
     {
